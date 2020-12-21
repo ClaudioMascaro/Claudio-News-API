ee41b776b31936602738e27440abb593d5e48598
Claudio Mascaro <claudiormascaro@gmail.com>
2020-12-21 17:42:32 -0300

    Config.ts e importando .mongodb no banco de dados

---
+++ src/config/config.ts
@@ -0,0 +1,3 @@
+export const config = {
+  mongodb: 'mongodb://localhost:27017/claudionews'
+};
--- src/database/index.ts
+++ src/database/index.ts
@@ -1,7 +1,8 @@
 import * as mongoose from 'mongoose'
+import { config } from '../config/config'
 
-mongoose.connect("mongodb://localhost:27017/claudionews", {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true}).then((db) => {
-    console.log("MongoDB conectado")
+mongoose.connect(config.mongodb, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true}).then((db) => {
+    console.log("Conectado com a base de dados!")
 }).catch((err) =>{
     console.log("Erro ao conectar com a base de dados")
 })
