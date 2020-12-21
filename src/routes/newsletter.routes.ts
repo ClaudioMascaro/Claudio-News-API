0c8fdcfba02a34f1510c2d725435057fdce77d78
Claudio Mascaro <claudiormascaro@gmail.com>
2020-12-21 14:07:35 -0300

    Método POST na rota newsletter (cru)

--- src/routes/newsletter.routes.ts
+++ src/routes/newsletter.routes.ts
@@ -1,7 +1,20 @@
 import { Router } from 'express'
+import mongoose from '../database'
+import '../models/Mailing'
 
+const Mailing = mongoose.model('Mailing')
 const newsletterRouter = Router()
 
-
+newsletterRouter.post('/', async (request, response) => {
+  try {
+    const { email } = request.body
+    const mailing = new Mailing ({email: email})
+    await mailing.save()
+    response.status(201).json({message: "E-mail cadastrado com sucesso."})
+  }
+  catch (err) {
+    response.status(400).json({ message: "E-mail já cadastrado!" })
+  }
+})
 
 export default newsletterRouter
