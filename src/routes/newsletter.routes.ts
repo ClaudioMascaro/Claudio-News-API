1305b9b29e2180d5d205e10f9728b9207aef4a6d
Claudio Mascaro <claudiormascaro@gmail.com>
2020-12-21 22:11:26 -0300

    Método post na rota newsletter

--- src/routes/newsletter.routes.ts
+++ src/routes/newsletter.routes.ts
@@ -1,7 +1,19 @@
 import { Router } from 'express'
+import Newsletter from '../controllers/mailingController'
 
 const newsletterRouter = Router()
 
+newsletterRouter.post('/', async (req, res) =>{
+  try {
+    const {email} = req.body
+    const newsletterSubscription = new Newsletter()
+    await newsletterSubscription.include(email)
+    res.status(201).json({ message: "E-mail cadastrado com sucesso!"})
+  }
+  catch (err) {
+    res.status(400).json({ message: "E-mail já cadastrado!" })
+  }
+})
 
 
 export default newsletterRouter
